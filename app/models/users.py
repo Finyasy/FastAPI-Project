557a4cb7be3298593ef483e66b6e95702abd99ff
Bryan Bosire <bryanbosire@gmail.com>
2022-07-03 01:25:44 +0300

    created relationship provided by SQLAlchemy ORM

--- app/models/users.py
+++ app/models/users.py
@@ -1,14 +1,16 @@
-from db.config import Base
-from sqlalchemy import Table,Column,Integer,String,ForeignKey,MetaData,DateTime,BigInteger,Float,func,Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from sqlalchemy import func
 from sqlalchemy.orm import relationship
+from db.config import Base
 
 class UserModel(Base):
     __tablename__ = 'users'
-    id=Column(Integer,primary_key=True)
-    username=Column(String,unique=True,nullable=False)
-    email=Column(String,unique=True,nullable=False)
-    password=Column(String,unique=True,nullable=False)
-    created_on=Column(DateTime(timezone=True),default=func.now(),nullable=True)
-    active=Column(Boolean,nullable=False,default=True)
-    tasks=relationship('TaskModel',back_populates='Owner')
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, unique=True, nullable=False)
+    created_on = Column(DateTime(timezone=True), default=func.now(), nullable=True)
+    active = Column(Boolean, nullable=False, default=True)
+    tasks = relationship('TaskModel', back_populates='Owner')
+    
