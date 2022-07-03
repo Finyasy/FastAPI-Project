522b93b261917b319190aabaeaed9b23d9b83ecd
Bryan Bosire <bryanbosire@gmail.com>
2022-07-03 12:00:17 +0300

    fixed typo errors and the class Config

--- app/schemas/task_schema.py
+++ app/schemas/task_schema.py
@@ -1,17 +1,17 @@
 from pydantic import BaseModel
 
 class TaskBase(BaseModel):
-    title:str
-    note:str
-    user_id:int
+    title: str
+    note: str
+    user_id: int
 
 class TaskCreate(TaskBase):
     pass
 
 class TaskOut(TaskBase):
-    id:int
-    completed:bool
-    created_on:str
+    id: int
+    completed: bool
+    created_on: str
 
     class Config:
-        orm_mode=True
+        orm_mode = True
